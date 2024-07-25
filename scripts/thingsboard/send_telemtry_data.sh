413adc6d34290e1e088ebd277907c0ccf309f96f
Muhammad Kamran <muhammmad.kamran@tuni.fi>
2024-07-25 17:12:21 +0300

    Take parameter from user in telemetry script

--- scripts/thingsboard/send_telemtry_data.sh
+++ scripts/thingsboard/send_telemtry_data.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 source env/bin/activate
-mosquitto_pub -d -q 1 -h demo.thingsboard.io -p 1883 -t esp/telemetry -u "WvamlBFvRGYgGsM7VN1u" -m "{temperature:21}"
+
+read -p "Enter your access token: " ACCESS_TOKEN
+read -p "Enter the parameter name: " PARAMETER_NAME
+read -p "Enter the parameter value: " PARAMETER_VALUE
+
+mosquitto_pub -d -q 1 -h demo.thingsboard.io -p 1883 -t esp/telemetry -u "$ACCESS_TOKEN" -m "{\"$PARAMETER_NAME\":$PARAMETER_VALUE}"
